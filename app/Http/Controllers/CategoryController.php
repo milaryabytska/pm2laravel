065a7c1f1572eb8ebf2253731ec1ade27af4a56d
milaryabytska <milana040704@gmail.com>
2022-11-07 13:23:51 +0200

    refactor: move business logic from controller to service

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,49 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\JsonResponse;
 use App\Models\Category;
 use App\Http\Requests\StoreCategoryRequest;
-use App\Htpp\Request\UpdateCategoryReqiest;
+use App\Http\Requests\UpdateCategoryRequest;
+use Illuminate\Http\JsonResponse;
+use App\Services\CategoryService;
+
 
 class CategoryController extends Controller
 {
-    public function index()
-    {
-        return response()->json(Category::all());
+    private CategoryService $service;
 
+    public function __construct(CategoryService $service)
+    {
+        $this->service = $service;
     }
+
+    public function index(): JsonResponse
+    {
+        return $this->service->index();
+    }
+
     public function store(StoreCategoryRequest $request): JsonResponse
     {
-        $data = $request->all();
-        
-        Category::create([
-            'name'=>$data['name'],
-            'description'=>$data['description'],
-            'type'=>$data['type']
-        ]);
-
-
-        return response()->json(Category::latest()->first()->get());
-        
-        
+        return $this->service->store($request->validated());
     }
 
-    public function update(StoreCategoryRequest $request, Category $category)
+    public function update(UpdateCategoryRequest $request, Category $category): JsonResponse
     {
-        $category->name = $request['name'];
-        $category->description = $request['description'];
-        $category->type = $request['type'];
-
-        $category->save();
-
-        return response()->json($category, 201);
+        return $this->service->update($request->validated(), $category);
     }
 
-    
     public function destroy(Category $category): JsonResponse
     {
         return response()->json($category->delete());
-
     }
 }
